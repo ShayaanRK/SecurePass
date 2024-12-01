cdd020a22865eee425f3ab69971270f35c0dbfee
Redowan Uddin <rafsan.raduwan@gmail.com>
2024-12-01 17:52:42 +0100

    Removing all templates

---
+++ src/components/FormTest.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <form>Hi</form>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'FormTest',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+</style>
