0bb6f7a9ffae412a8243fa2ac2837b2d554421c4
Redowan Uddin <rafsan.raduwan@gmail.com>
2025-02-26 20:24:33 +0100

    feat: added a Password Generator

--- src/components/FormTest.vue
+++ src/components/FormTest.vue
@@ -1,17 +1,38 @@
 <template>
-    <div>
-        <form>Hi</form>
+  <div>
+    <form @submit.prevent="handleSubmit">
+      <label for="length">Password Length:</label>
+      <input type="number" v-model="length" min="1" max="128" required />
+      <button type="submit">Generate Password</button>
+    </form>
+    <div v-if="password">
+      <h3>Generated Password:</h3>
+      <p>{{ password }}</p>
     </div>
+  </div>
 </template>
 
 <script>
+import { generatePassword } from '../utils/passwordGenerator';
+
 export default {
   name: 'FormTest',
-  props: {
-    msg: String
+  data() {
+    return {
+      length: 12,
+      password: ''
+    };
+  },
+  methods: {
+    handleSubmit() {
+      this.password = generatePassword(this.length);
+    }
   }
-}
+};
 </script>
 
-<style>
+<style scoped>
+form {
+  margin-bottom: 20px;
+}
 </style>
