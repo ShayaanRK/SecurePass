8f7be272b7c834d42dd3d4fcf3c9111cb64d11bd
Redowan Uddin <rafsan.raduwan@gmail.com>
2025-03-02 20:38:40 +0100

    feat: added local host storage features

--- src/components/FormTest.vue
+++ src/components/FormTest.vue
@@ -7,15 +7,16 @@
     <div>
       <form @submit.prevent="handleSubmit">
         <label for="length">Password Length:</label>#
-        <input type="number" v-model="length" min="1" max="128" required />
+        <input type="number" v-model="length" min="6" max="30" required />
         <button type="submit">Generate Password</button>
       </form>
     </div>
 
     <div v-if="password">
       <h3>Generated Password:</h3>
-      <p>{{ password }}</p>
+      <p>{{ password }} {{ siteName }} </p>
       <button @click="handleDownload">Download Password</button>
+      <button @click="addPassword">Add Password</button>
     </div>
   </div>
 </template>
@@ -30,19 +31,27 @@ export default {
     return {
       length: 12,
       password: '',
-      site: '',
       siteName: 'test',
     };
   },
   methods: {
     handleSubmit() {
-      this.password = generatePassword(this.length);
-      this.site = this.siteName;
+      const newPassword = generatePassword(this.length);
+      this.password = newPassword;
     },
     handleDownload() {
       downloadPassword(this.password, this.site);
     },
-  }
+    addPassword() {
+      localStorage.setItem(this.siteName, this.password);
+
+      alert('Password added successfully');
+      console.log(localStorage.getItem('locker'));
+    },
+  },
+  onMounted() {
+    this.passwords = JSON.parse(localStorage.getItem('locker')) || {};
+  },
 };
 </script>
 
